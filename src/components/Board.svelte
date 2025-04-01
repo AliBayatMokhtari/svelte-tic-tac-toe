e28011ac3dd5aed2864ba7873dd73b811b595ad3
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2025-04-02 00:06:08 +0330

    Reset game

--- src/components/Board.svelte
+++ src/components/Board.svelte
@@ -12,15 +12,31 @@
 		arr = [...newArr];
 		turn = turn === 'X' ? 'O' : 'X';
 	}
+
+	function resetGame() {
+		arr = Array.from({ length: 9 }).map(() => undefined);
+		turn = 'X';
+	}
 </script>
 
-<div class="mt-4 grid h-[300px] w-[300px] grid-cols-3 border">
-	{#each arr as value, i}
-		<Cell
-			{value}
-			pos={i}
-			onClick={onCellClick}
-		/>
-	{/each}
-	<span>{result}</span>
+<div>
+	<div class="mt-4 grid h-[300px] w-[300px] grid-cols-3 border">
+		{#each arr as value, i}
+			<Cell
+				{value}
+				pos={i}
+				onClick={onCellClick}
+				disabled={value || result}
+			/>
+		{/each}
+	</div>
+	<div class="mt-2">
+		{#if result}
+			<div class="text-center text-2xl">{result === 'Draw' ? result : `Winner is: ${result}`}</div>
+		{/if}
+		<button
+			onclick={resetGame}
+			class="mt-2 w-full cursor-pointer rounded-sm bg-gray-200 py-1">Reset Game</button
+		>
+	</div>
 </div>
--- src/components/Cell.svelte
+++ src/components/Cell.svelte
@@ -1,12 +1,12 @@
 <script>
 	import { getCellClass } from '$lib';
 
-	let { value, pos, onClick } = $props();
+	let { value, pos, disabled, onClick } = $props();
 </script>
 
 <button
+	{disabled}
 	class={getCellClass(pos)}
-	disabled={value}
 	onclick={() => onClick(pos)}
 >
 	{value}
